62ad03ac4a00d7cc67e72916a521aade39aca022
ThibautCadiou <cadioucontact@gmail.com>
2024-07-18 11:54:57 +0200

    sauvegarde avant de tout crasher avec l'ajout de la sécurité sur les validators de mdp et d'adresse mail

--- models/userModel.js
+++ models/userModel.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, 'A user must have an email'],
     unique: true,
+    validate: [validator.isEmail, 'Please provide a valid E-mail'],
   },
   password: {
     type: String,
     required: [true, 'A Book must have a password'],
     unique: true,
+    minLength: 8,
   },
 });
 
