5a2c84f64adb39fcbe42e77a5fc6de9d7f4a9b04
ThibautCadiou <cadioucontact@gmail.com>
2024-07-18 16:58:21 +0200

    clean de server.js et congi.env plus ajout d'une DB prod et dev

--- server.js
+++ server.js
@@ -3,20 +3,27 @@ const app = require('./App');
 
 const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
-const DB_BOOKS = process.env.DATABASE_BOOKS.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+let DB_BOOKS = process.env.DATABASE_BOOKS.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+if (process.env.NODE_ENV === 'development') {
+  console.log('\n\n MODE DEVELOPPEMENT');
+  DB_BOOKS = process.env.DATABASE_BOOKS_DEV.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+} else if (process.env.NODE_ENV === 'production') {
+  console.log('\n\n MODE PRODUCTION');
+  DB_BOOKS = process.env.DATABASE_BOOKS_PRODUCTION.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+  );
+}
 
 dbConnect().catch((err) => console.log(err));
 
 async function dbConnect() {
   await mongoose.connect(DB_BOOKS);
-  console.log('DB connection sucessful');
-}
-
-if (process.env.NODE_ENV === 'development') {
-  // console.log(process.env);
+  console.log('DB connection successful');
 }
 
 const port = process.env.PORT;
 app.listen(port, () => {
-  console.log(`Running on port: ${port} ...`);
+  console.log(`\n\nRunning on port: ${port} ...`);
 });
